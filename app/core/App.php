79d99825c38b7721072cce07c039a9f9b925be4b
ft <>
2021-10-01 15:43:16 +0700

    Menyelesaikan video 3 (Routing)

--- app/core/App.php
+++ app/core/App.php
@@ -1,10 +1,21 @@
 <?php
 //Pertemuan 4
-//Video 2
+
 class App
 {
   public function __construct()
   {
-    echo 'OK!';
+    $url = $this->parseURL();
+    var_dump($url);
+  }
+
+  public function parseURL()
+  {
+    if (isset($_GET['url'])) {
+      $url = rtrim($_GET['url']);
+      $url = filter_var($url, FILTER_SANITIZE_URL);
+      $url = explode('/', $url);
+      return $url;
+    }
   }
 }
