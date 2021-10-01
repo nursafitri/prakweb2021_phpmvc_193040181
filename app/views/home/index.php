0373d46591aafcca46e5d666a87674443be960ed
ft <>
2021-10-01 21:08:45 +0700

    Menyelesaikan video 7 (Model)

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -2,6 +2,7 @@
   <div class="jumbotron">
     <div class="jumbotron mt-4">
       <h1 class="display-4">Selamat datang di website saya</h1>
+      <p class="lead">Halo, nama saya <?= $data['nama']; ?></p>
       <hr class="my-4">
       <p>THE FEELS.</p>
       <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
