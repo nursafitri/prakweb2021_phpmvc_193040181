3b82c06f7d37c99a0e84352881fa4d25d6080527
ft <>
2021-10-01 20:39:00 +0700

    Menyelesaikan video 6 (Assets)

--- app/init.php
+++ app/init.php
@@ -1,5 +1,6 @@
 <?php
 //Pertemuan 4
-//Video 2
+
 require_once 'core/App.php';
 require_once 'core/Controller.php';
+require_once 'core/Constants.php';
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,13 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <title>Halaman Home</title>
-</head>
-
-<body>
-  <h1>Selamat Datang di Website Saya</h1>
-</body>
-
-</html>
+<div class="container">
+  <div class="jumbotron">
+    <div class="jumbotron mt-4">
+      <h1 class="display-4">Selamat datang di website saya</h1>
+      <hr class="my-4">
+      <p>THE FEELS.</p>
+      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
+  </div>
